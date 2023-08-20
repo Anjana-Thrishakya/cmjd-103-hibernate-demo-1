6e92aca9b2ae33f86e515f2a1cc554eb58936bf4
Anjana Thrishakya <anjanathrishakya@gmail.com>
2023-08-20 11:56:06 +0530

    create student table with id and nic

--- src/util/SessionFactoryConfiguartion.java
+++ src/util/SessionFactoryConfiguartion.java
@@ -4,13 +4,16 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import entity.StudentEntity;
+
 public class SessionFactoryConfiguartion {
     private static SessionFactoryConfiguartion sessionFactoryConfiguartion;
 
     private SessionFactory sessionFactory;
 
     private SessionFactoryConfiguartion(){
-        Configuration configuration = new Configuration().configure();
+        Configuration configuration = new Configuration().configure()
+        .addAnnotatedClass(StudentEntity.class);
         sessionFactory = configuration.buildSessionFactory();
     }
 
